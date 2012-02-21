1f061243bb5d559b4d5058925eee333373935300
Pikachu <mailforspam@meta.ua>
2012-02-21 15:05:01 +0200

    add views(Sonik design)

---
+++ www/application/views/DeviceInfo_files/axurerp_pagespecificscript.js
@@ -0,0 +1,128 @@
+﻿
+var PageName = 'DeviceInfo';
+var PageId = '40366cb4975340a0b5eafaa36b698570'
+var PageUrl = 'DeviceInfo.html'
+document.title = 'DeviceInfo';
+var PageNotes = 
+{
+"pageName":"DeviceInfo",
+"showNotesNames":"False"}
+var $OnLoadVariable = '';
+
+var $CSUM;
+
+var hasQuery = false;
+var query = window.location.hash.substring(1);
+if (query.length > 0) hasQuery = true;
+var vars = query.split("&");
+for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0].length > 0) eval("$" + pair[0] + " = decodeURIComponent(pair[1]);");
+} 
+
+if (hasQuery && $CSUM != 1) {
+alert('Prototype Warning: The variable values were too long to pass to this page.\nIf you are using IE, using Firefox will support more data.');
+}
+
+function GetQuerystring() {
+    return '#OnLoadVariable=' + encodeURIComponent($OnLoadVariable) + '&CSUM=1';
+}
+
+function PopulateVariables(value) {
+    var d = new Date();
+  value = value.replace(/\[\[OnLoadVariable\]\]/g, $OnLoadVariable);
+  value = value.replace(/\[\[PageName\]\]/g, PageName);
+  value = value.replace(/\[\[GenDay\]\]/g, '16');
+  value = value.replace(/\[\[GenMonth\]\]/g, '2');
+  value = value.replace(/\[\[GenMonthName\]\]/g, 'Февраль');
+  value = value.replace(/\[\[GenDayOfWeek\]\]/g, 'четверг');
+  value = value.replace(/\[\[GenYear\]\]/g, '2012');
+  value = value.replace(/\[\[Day\]\]/g, d.getDate());
+  value = value.replace(/\[\[Month\]\]/g, d.getMonth() + 1);
+  value = value.replace(/\[\[MonthName\]\]/g, GetMonthString(d.getMonth()));
+  value = value.replace(/\[\[DayOfWeek\]\]/g, GetDayString(d.getDay()));
+  value = value.replace(/\[\[Year\]\]/g, d.getFullYear());
+  return value;
+}
+
+function OnLoad(e) {
+
+}
+
+var u3 = document.getElementById('u3');
+gv_vAlignTable['u3'] = 'center';
+var u21 = document.getElementById('u21');
+gv_vAlignTable['u21'] = 'top';
+var u16 = document.getElementById('u16');
+gv_vAlignTable['u16'] = 'top';
+var u12 = document.getElementById('u12');
+gv_vAlignTable['u12'] = 'center';
+var u15 = document.getElementById('u15');
+gv_vAlignTable['u15'] = 'top';
+var u4 = document.getElementById('u4');
+
+var u0 = document.getElementById('u0');
+
+var u8 = document.getElementById('u8');
+gv_vAlignTable['u8'] = 'top';
+var u19 = document.getElementById('u19');
+gv_vAlignTable['u19'] = 'top';
+var u10 = document.getElementById('u10');
+
+var u17 = document.getElementById('u17');
+gv_vAlignTable['u17'] = 'top';
+var u5 = document.getElementById('u5');
+gv_vAlignTable['u5'] = 'center';
+var u1 = document.getElementById('u1');
+gv_vAlignTable['u1'] = 'center';
+var u9 = document.getElementById('u9');
+
+u9.style.cursor = 'pointer';
+if (bIE) u9.attachEvent("onclick", Clicku9);
+else u9.addEventListener("click", Clicku9, true);
+function Clicku9(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	SetPanelVisibility('u10','','none',500);
+
+}
+
+}
+gv_vAlignTable['u9'] = 'top';
+var u14 = document.getElementById('u14');
+gv_vAlignTable['u14'] = 'top';
+var u20 = document.getElementById('u20');
+gv_vAlignTable['u20'] = 'top';
+var u6 = document.getElementById('u6');
+
+var u2 = document.getElementById('u2');
+
+var u11 = document.getElementById('u11');
+
+u11.style.cursor = 'pointer';
+if (bIE) u11.attachEvent("onclick", Clicku11);
+else u11.addEventListener("click", Clicku11, true);
+function Clicku11(e)
+{
+windowEvent = e;
+
+
+if (true) {
+
+	SetPanelVisibility('u10','hidden','none',500);
+
+}
+
+}
+
+var u13 = document.getElementById('u13');
+gv_vAlignTable['u13'] = 'top';
+var u18 = document.getElementById('u18');
+gv_vAlignTable['u18'] = 'top';
+var u7 = document.getElementById('u7');
+gv_vAlignTable['u7'] = 'center';
+if (window.OnLoad) OnLoad();
---
+++ www/application/views/data/sitemap.js
@@ -0,0 +1,17 @@
+﻿var sitemap = 
+{
+"rootNodes":[
+{
+"pageName":"Home",
+"type":"Wireframe",
+"url":"Home.html",
+"children":[
+{
+"pageName":"AnswerList",
+"type":"Wireframe",
+"url":"AnswerList.html",
+"children":[
+{
+"pageName":"DeviceInfo",
+"type":"Wireframe",
+"url":"DeviceInfo.html"}]}]}]};
